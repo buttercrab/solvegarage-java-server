797f9cfd6d45f2082e49fc751a31071e1ce0bc88
buttercrab <jaeyong0201@gmail.com>
2019-06-18 01:20:54 +0900

    added problem posting

--- src/database/Post.java
+++ src/database/Post.java
@@ -38,4 +38,41 @@ public class Post {
         }
         return new Pair<>(false, 0);
     }
+
+    /**
+     * Make new Problem given.
+     * <p>
+     * Failure codes
+     * <ul>
+     * <li> 0: Server Error
+     * <li> 1: Id is not valid
+     * <li> 2: token is not valid
+     * </ul>
+     *
+     * @param id    id that made problem
+     * @param tk    token to verify
+     * @param title title of problem
+     * @param body  body of problem
+     * @return response data
+     */
+
+    public synchronized Pair<Boolean, Object> setProblem(String id, String tk, String title, String body) {
+        try {
+            ResultSet rs = Database.st.executeQuery("SELECT token FROM user WHERE id='" + id + "'");
+            if (rs.next()) {
+                String t = rs.getString("token");
+                if (!rs.wasNull() && t.equals(tk)) {
+                    t = Util.token.generate(id);
+                    Database.st.executeUpdate("UPDATE user SET token='" + t + "' WHERE id='" + id + "'");
+                    Database.st.executeUpdate("INSERT INTO post (type, title, author, date, body, tags, category) VALUES (1, '" + title + "', '" + id + "', curdate(), '" + body + "', '', '')");
+                    return new Pair<>(true, t);
+                }
+                return new Pair<>(false, 2);
+            }
+            return new Pair<>(false, 1);
+        } catch (SQLException e) {
+            Util.log("database", "Error on setting problem with id: " + id, Commands.ERR);
+        }
+        return new Pair<>(false, 0);
+    }
 }
--- src/server/ProblemHandler.java
+++ src/server/ProblemHandler.java
@@ -1,10 +1,12 @@
 package server;
 
+import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import javafx.util.Pair;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Base64;
 
 public class ProblemHandler extends SecureHttpHandler {
 
@@ -12,6 +14,10 @@ public class ProblemHandler extends SecureHttpHandler {
      * This method is used for handling problem requests.
      * Using GET Method, it sends JSON object that contains
      * `success` property
+     * <p>
+     * Using POST Method, it gets the new problem and saves
+     * to the database and sends back response JSON object
+     * that contains `success` property.
      *
      * @param exchange http exchange object
      * @throws IOException when something goes wrong
@@ -26,9 +32,9 @@ public class ProblemHandler extends SecureHttpHandler {
             String res = "{'success':" + t.getKey();
             if (t.getKey()) {
                 String[] s = (String[]) t.getValue();
-                res += ",'title':'" + s[0] + "'";
+                res += ",'title':'" + Base64.getEncoder().encodeToString(s[0].getBytes()) + "'";
                 res += ",'author':'" + s[1] + "'";
-                res += ",'body':'" + s[2] + "'";
+                res += ",'body':'" + Base64.getEncoder().encodeToString(s[2].getBytes()) + "'";
                 res += ",'date':'" + s[3] + "'}";
             } else
                 res += ",'code':" + t.getValue() + "}";
@@ -38,7 +44,22 @@ public class ProblemHandler extends SecureHttpHandler {
             os.write(res.getBytes());
             os.close();
         } else {
+            Pair<JsonObject, Byte[]> root = super.handleInit(exchange);
+            if (root == null) return;
 
+            String id = root.getKey().get("id").getAsString();
+            String tk = root.getKey().get("token").getAsString();
+            String title = new String(Base64.getDecoder().decode(root.getKey().get("title").getAsString()));
+            String body = new String(Base64.getDecoder().decode(root.getKey().get("body").getAsString()));
+
+            Pair<Boolean, Object> t = Server.post.setProblem(id, tk, title, body);
+            String res = "{'success':" + t.getKey();
+            if (t.getKey())
+                res += ",'token':'" + t.getValue() + "'}";
+            else
+                res += ",'code':" + t.getValue() + "}";
+
+            super.send(exchange, res, root.getValue());
         }
     }
 }
